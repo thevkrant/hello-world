50ed8a1c4da92c5ad51a75c5d55c4aca38cacd3f
Gerardo Alcantara <32178037+GeraAlcantara@users.noreply.github.com>
2022-10-01 13:17:52 -0500

    svelte helloworld and this show the libary sintax

---
+++ s/svelte.svelte
@@ -0,0 +1,20 @@
+<!-- this is like jsx but split -->
+<script>
+    const sayHiSvelte = "Hello Wold";
+    /* say hi to console */
+    console.log(sayHiSvelte);
+</script>
+
+<div class="container">
+    <h1>{sayHiSvelte}</h1>
+</div>
+
+<style>
+    .container {
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
